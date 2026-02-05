2ae3459a042a4b959d64734190bf1a0617bddee4
MD Rashidul Islam <50497898+mrirashid@users.noreply.github.com>
2026-02-05 19:14:07 +0800

    Delete frontend/health directory

--- frontend/health/src/utils/fakeApi.ts
+++
@@ -1,7 +0,0 @@
-export function delay<T>(data: T, ms = 300): Promise<T> {
-  return new Promise((resolve) => setTimeout(() => resolve(data), ms))
-}
-
-export function uid(prefix = 'id'): string {
-  return `${prefix}_${Math.random().toString(36).slice(2, 9)}`
-}
